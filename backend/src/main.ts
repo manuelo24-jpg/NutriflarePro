0774797335b195f9392fec0ecf0cbb12250ef5ed
manuelo24-jpg <manullanorebanal@gmail.com>
2026-06-05 11:57:57 +0200

    arreglo de errores

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -18,6 +18,6 @@ async function bootstrap() {
     transform: true,
   }));
 
-  await app.listen(process.env.PORT ?? 3001);
+  await app.listen(process.env.PORT ?? 3001, '0.0.0.0');
 }
 bootstrap();
